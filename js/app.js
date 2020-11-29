9ab1c12ceb89af2826c74b3faeb522145d5ba6ad
Ghada Fahad <gasker@tetco.sa>
2020-11-29 05:49:33 +0300

    Set the default active menu item

--- js/app.js
+++ js/app.js
@@ -31,22 +31,30 @@
  */
 
 // build the nav
-const nav = document.querySelector("#navbar__list");
-const sections = document.querySelectorAll("section[data-nav]");
-const navItemsFragment = document.createDocumentFragment();
+function buildNavigationMenu() {
+  const nav = document.querySelector("#navbar__list");
+  const sections = document.querySelectorAll("section[data-nav]");
+  const navItemsFragment = document.createDocumentFragment();
 
-for (const section of sections) {
-  let navItem = document.createElement("li");
-  let navLink = document.createElement("a");
+  for (let i = 0; i < sections.length; i++) {
+    let navItem = document.createElement("li");
+    let navLink = document.createElement("a");
 
-  navLink.setAttribute("class", "menu__link");
-  navLink.textContent = section.getAttribute("data-nav");
+    // Setting up the first item in the menu as active by default
+    if (i === 0) {
+      navLink.setAttribute("class", "menu__link menu__item__active");
+    } else {
+      navLink.setAttribute("class", "menu__link");
+    }
 
-  navItem.appendChild(navLink);
-  navItemsFragment.appendChild(navItem);
+    navLink.textContent = sections[i].getAttribute("data-nav");
+
+    navItem.appendChild(navLink);
+    navItemsFragment.appendChild(navItem);
+  }
+
+  nav.appendChild(navItemsFragment);
 }
-
-nav.appendChild(navItemsFragment);
 
 // Add class 'active' to section when near top of viewport
 
